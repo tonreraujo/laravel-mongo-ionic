765938776be11ba23b3d6552c62638160ea40a6f
elton <eltonaraujo.br@gmail.com>
2018-03-16 21:23:36 -0300

    adicionando aula 4

---
+++ aula4/database/migrations/2018_03_15_014909_create_usuario_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->integer('idade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
